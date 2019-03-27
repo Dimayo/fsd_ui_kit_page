aac697fd14cd62faa310af890fb7efd95d3b9fb9
Dima Mayorkin <44707838+Dimayo@users.noreply.github.com>
2019-03-27 16:30:14 +0600

    Function correction

--- src/components/pie-chart/pie-chart.js
+++ src/components/pie-chart/pie-chart.js
@@ -1,18 +1,13 @@
 function pieChart() {
+    let ring = document.querySelectorAll('.pie-chart__bar');
+    let percentage = document.querySelectorAll('.pie-chart__percent');
 
-    let ring = document.querySelectorAll('.pie-chart__bar'),
-        percentage = document.querySelectorAll('.pie-chart__percent');
+    const percentageToDashOffset = (circumference, n) => circumference / 100 * (100 - n);
+    const updateDonutValue = (donut, circumference, n) => donut.setAttribute('stroke-dashoffset', percentageToDashOffset(circumference, n));
 
     for (let i = 0; i < ring.length; i++) {
         let circumference = 2 * Math.PI * ring[i].getAttribute('r');
-        let percentageToDashOffset = (n) => {
-            return (circumference / 100) * (100 - n);
-        };
-
-        let updateDonutValue = (n) => {
-            ring[i].setAttribute('stroke-dashoffset', percentageToDashOffset(n));
-        };
-        return updateDonutValue(percentage[i].textContent);
+        updateDonutValue(ring[i], percentage[i].textContent);
     }
 }
 
