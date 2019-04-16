227983f8731bb13a116e9071ca8a581be9de5fd1
Dima Mayorkin <44707838+Dimayo@users.noreply.github.com>
2019-04-16 17:35:38 +0600

    Calendar finished

--- src/components/calendar/calendar.js
+++ src/components/calendar/calendar.js
@@ -1,4 +1,4 @@
-var Calendar = (function () {
+const Calendar = (function () {
     function Calendar(container) {
         if (container === void 0) {
             container = 'calendar';
@@ -17,9 +17,9 @@ var Calendar = (function () {
         this.container = container;
         this.showCalendar();
     }
-    //showCalendar();
+
     Calendar.prototype.showCalendar = function () {
-        var container = document.getElementById(this.container);
+        const container = document.getElementById(this.container);
         if (container) {
             var table = document.createElement('table');
             table.classList.add('calendar__table');
@@ -30,52 +30,51 @@ var Calendar = (function () {
             container.appendChild(table);
         }
     };
-    //calendarHeader
-    Calendar.prototype.calendarHeader = function () {
-        var _this = this;
-        //create calendar header
-        var calendarHeader = document.createElement('thead');
-        calendarHeader.classList.add('calendar__header');
-        var navDay = document.createElement('tr');
-        var navRow = document.createElement('tr');
-        navRow.classList.add('calendar__nav');
-        //get current year and month
 
-        var number = document.createElement('td');
+    Calendar.prototype.calendarHeader = function () {
+        const _this = this;
+        const calendarHeader = document.createElement('thead');
+        calendarHeader.classList.add('calendar__header');
+        const navDay = document.createElement('tr');
+        const navRow = document.createElement('tr');
+        navRow.classList.add('calendar__nav');
+
+
+        const number = document.createElement('td');
         number.setAttribute('colspan', '7');
         number.classList.add('calendar__date');
         navDay.appendChild(number);
         number.innerHTML = this.currentDay;
 
-        var prev = document.createElement('td');
+        const prev = document.createElement('td');
         prev.classList.add('prev');
         prev.innerHTML = '<span class="calendar__left-arrow"></span>';
         navRow.appendChild(prev);
-        //prev action
+
         prev.addEventListener('click', function (e) {
             _this.previous();
             _this.showCalendar();
         });
-        var current = document.createElement('td');
+        const current = document.createElement('td');
         this.monthAndYear = this.months[this.selectMonth];
         current.setAttribute('colspan', '5');
         current.classList.add('calendar__month');
         current.appendChild(document.createTextNode(this.monthAndYear));
         navRow.appendChild(current);
-        var next = document.createElement('td');
+        const next = document.createElement('td');
         next.classList.add('next');
         next.innerHTML = '<span class="calendar__right-arrow"></span>';
         navRow.appendChild(next);
-        //next action
+
         next.addEventListener('click', function (e) {
             _this.next();
             _this.showCalendar();
         });
-        //get days calendar
-        var headerDays = document.createElement('tr');
+
+        const headerDays = document.createElement('tr');
         headerDays.classList.add('calendar__days-name');
-        for (var i = 0; i < this.days.length; i++) {
-            var daysCell = document.createElement('td');
+        for (let i = 0; i < this.days.length; i++) {
+            const daysCell = document.createElement('td');
             daysCell.appendChild(document.createTextNode(this.days[i]));
             headerDays.appendChild(daysCell);
         }
@@ -84,11 +83,12 @@ var Calendar = (function () {
         calendarHeader.appendChild(headerDays);
         return calendarHeader;
     };
+
     Calendar.prototype.calendarFooter = function () {
-        var _this = this;
-        var tr = document.createElement('tr');
+        const _this = this;
+        const tr = document.createElement('tr');
         tr.classList.add('calendar__footer');
-        var today = document.createElement('td');
+        const today = document.createElement('td');
         today.setAttribute('colspan', '7');
         today.classList.add('calendar__today-button');
         today.innerHTML = 'Today';
@@ -99,64 +99,58 @@ var Calendar = (function () {
         });
         return tr;
     };
+
     Calendar.prototype.calendarBody = function () {
-        var _this = this;
-        //get calendar body
-        var firstDay = (new Date(this.selectYear, this.selectMonth)).getDay();
-        var lastDay = (new Date(this.selectYear, this.selectMonth + 1, 0)).getDate();
-        var lastDayOfLastMonth = this.selectMonth == 0 ? new Date(this.selectYear - 1, 11, 0).getDate() : new Date(this.selectYear, this.selectMonth, 0).getDate();
-        var tbl = document.createElement('tbody');
+        const _this = this;
+        const firstDay = (new Date(this.selectYear, this.selectMonth)).getDay();
+        const tbl = document.createElement('tbody');
         tbl.classList.add('calendar__body');
         tbl.innerHTML = "";
-        // filing data about month and in the page via DOM.
         this.selectYear = this.currentYear;
         this.selectMonth = this.currentMonth;
-        // creating all cells
-        var date = 1;
-        for (var i = 0; i < 6; i++) {
-            // creates a table row
-            var row = document.createElement("tr");
+        let date = 1;
+        for (let i = 0; i < 5; i++) {
+            const row = document.createElement("tr");
             row.classList.add('calendar__row');
-            var _loop_1 = function (j) {
+            const loop = function (j) {
                 if (i === 0 && j < firstDay - 1) {
-                    var cell = document.createElement("td");
-                    var cellText = document.createTextNode("");
-                    cell.appendChild(cellText);
-                    row.appendChild(cell);
-                } else if (date > this_1.daysInMonth(this_1.selectMonth, this_1.selectYear)) {
-                    return "break";
-                }else {
-                    var cell = document.createElement("td");
+                    const cell = document.createElement("td");
                     cell.classList.add('calendar__cell');
-                    var cellText = document.createTextNode('' + date + '');
-                    if (date === this_1.today.getDate() && this_1.selectYear === this_1.today.getFullYear() && this_1.selectMonth === this_1.today.getMonth()) {
+                    row.appendChild(cell);
+                } else {
+                    const cell = document.createElement("td");
+                    cell.classList.add('calendar__cell');
+                    if (date === thisOne.today.getDate() && thisOne.selectYear === thisOne.today.getFullYear() && thisOne.selectMonth === thisOne.today.getMonth()) {
                         cell.innerHTML = date;
                         cell.classList.add('calendar__day');
                         cell.classList.add('calendar__today');
                         cell.classList.add('calendar__day_selected');
-                    } // color today's date
-                    else {
+                    } else if (date < thisOne.daysInMonth(thisOne.selectMonth, thisOne.selectYear)) {
                         cell.innerHTML = date;
                         cell.classList.add('calendar__day');
+                    } else {
+                        cell.classList.add('calendar__day');
                     }
-                    var id_1 = this_1.selectYear + '-' + ((this_1.selectMonth < 10) ? '0' + (this_1.selectMonth + 1) : this_1.selectMonth + 1) + '-' + (date < 10 ? '0' + date : date);
-                    cell.setAttribute('id', id_1);
-                    //next action
+
+                    const idOne = date < 10 ? '0' + date : date;
+                    cell.setAttribute('id', idOne);
+
                     cell.addEventListener('click', function (e) {
-                        _this.select(id_1);
+                        _this.select(idOne);
                     });
                     row.appendChild(cell);
                     date++;
                 }
             };
-            var this_1 = this;
-            //creating individual cells, filing them up with data.
-            for (var j = 0; j < 7; j++) {
-                var state_1 = _loop_1(j);
-                if (state_1 === "break")
+            const thisOne = this;
+
+            for (let j = 0; j < 7; j++) {
+                const stateOne = loop(j);
+                if (stateOne === "break") {
                     break;
+                }
             }
-            tbl.appendChild(row); // appending each row into calendar body.
+            tbl.appendChild(row);
         }
         return tbl;
     };
@@ -176,15 +170,15 @@ var Calendar = (function () {
     };
     Calendar.prototype.select = function (id) {
         this.selectDate = id;
-        var els = document.querySelectorAll('.calendar__day_selected');
+        const els = document.querySelectorAll('.calendar__day_selected');
         for (var i = 0; i < els.length; i++) {
             els[i].classList.remove('calendar__day_selected');
         }
-        var x = document.getElementById(id);
+        const x = document.getElementById(id);
         x.classList.add('calendar__day_selected');
 
-        var selected = document.querySelector('.calendar__day_selected');
-        var number = document.querySelector('.calendar__date');
+        const selected = document.querySelector('.calendar__day_selected');
+        const number = document.querySelector('.calendar__date');
         number.innerHTML = selected.innerHTML;
     };
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
-import './components/video-frame/video-frame.js';
-import './components/video-frame/expand.svg';
-import './components/video-frame/play.svg';
-import './components/video-frame/pause.svg';
-import './components/video-frame/background.jpg';
+import './components/calendar/calendar.js';
+
 
 
 
